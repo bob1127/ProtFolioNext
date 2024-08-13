8225903e1d71393018fffa84fb858ea7154bf778
bob1127 <61307295+bob1127@users.noreply.github.com>
2024-08-13 16:44:48 +0800

    ooo

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
 
   images: {
     domains: [
-      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
+    
       'i0.wp.com',
       '1.gravatar.com',
       '2.gravatar.com',
