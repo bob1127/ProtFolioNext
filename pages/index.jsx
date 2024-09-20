1d800efdf5a7ff60d7a4ed732c5d78b4393eb38f
bob1127 <61307295+bob1127@users.noreply.github.com>
2024-09-20 20:43:48 +0800

    emmm

--- pages/index.jsx
+++ pages/index.jsx
@@ -56,7 +56,7 @@ export default function Home() {
 
   return (
     <div className="">
-      <>
+      <div>
         <NextSeo
           title="極客網頁設計｜JEEK WEBDESIGN｜形象官網、商業攝影、客製化網站、套版網站 - 你的創意，我來實踐"
           description="JEEK 專注於網頁設計和網頁結構優化，致力於為您打造視覺美觀、使用者友好的網站。通過精確的設計和優化策略，我們提升網站的加載速度和使用者體驗，幫助您的品牌在數位世界中脫穎而出。選擇 JEEK，讓您的線上存在更具吸引力和實用性。"
@@ -621,7 +621,7 @@ CREACTIVEs</h1>
 
         {/* <Slider/> */}
         {/* <CarouselImg/> */}
-      </>
+      </div>
     </div>
   );
 }
