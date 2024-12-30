ebde8832c8caf3aa43c0733c7e3a8692d1ce3915
bob1127 <61307295+bob1127@users.noreply.github.com>
2024-12-30 11:19:51 +0800

    sed

---
+++ pages/LandingPage/index.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Hero from "../../components/LandingPage/Hero/page";
+import Grid from "../../components/LandingPage/Grid/page";
+// import Footer from "@/components/Footer";
+// import Clients from "@/components/Clients";
+// import Approach from "@/components/Approach";
+// import Experience from "@/components/Experience";
+import RecentProjects from "../../components/LandingPage/RecentProjects/page";
+// import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
+const Home = () => {
+  return (
+    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        {/* <Clients /> */}
+      </div>
+    </main>
+  );
+};
+
+export default Home;
